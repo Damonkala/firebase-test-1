fc26df19c540ced1cf77f1508857f2d1700430ad
cnichols <mellowdious@mail.com>
2015-11-04 14:42:42 -0800

    pushing objects

--- main.js
+++ main.js
@@ -1,25 +1,39 @@
 'use strict';
 
 var ref = new Firebase("https://cadesapp.firebaseio.com/");
+var name;
 
 $(document).ready(init);
 
 function init() {
   $('#add').click(addMessage);
+  $("#start").click(start);
 
   ref.on('child_added', function(snapshot){
     var value = snapshot.val();
-    var $li = $('<li>').text(value);
+    console.log('value:', value);
+    var $li = $('<li>').text(value.time + ' ' + value.name + ' ' + value.message);
     $('#output').append($li);
   })
+}
 
-
+function start(){
+  name = $('#name').val();
+  $('#name').val('');
+  if(name.length > 0 && name.length < 15){
+    $('.newMessage').show();
+    $('.chooseName').hide();
+  } else {
+    alert('Invalid name.');
+  }
 }
 
 function addMessage(){
   var message = $("#message").val();
   $("#message").val('');
-
-  ref.push(message);
-
+  ref.push({
+    name: name,
+    time: Date.now(),
+    message: message
+  });
 }
