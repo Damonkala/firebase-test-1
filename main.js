75a6df4e29ae38bb24b8a2a149a0d2cb87598fa6
cnichols <mellowdious@mail.com>
2015-11-04 15:25:15 -0800

    users maintained

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 'use strict';
 
-var ref = new Firebase("https://cadesapp.firebaseio.com/");
+var ref = new Firebase("https://baconjelly.firebaseio.com/");
+
+var chatRef = ref.child('chat');
+var usersRef = ref.child('users');
+
 var name;
 
 $(document).ready(init);
@@ -8,21 +12,55 @@ $(document).ready(init);
 function init() {
   $('#add').click(addMessage);
   $("#start").click(start);
+  $("#logout").click(logout);
+  
+  $(window).on('beforeunload', logout)
 
-  ref.on('child_added', function(snapshot){
+  chatRef.on('child_added', function(snapshot){
     var value = snapshot.val();
-    console.log('value:', value);
     var $li = $('<li>').text(value.time + ' ' + value.name + ' ' + value.message);
     $('#output').append($li);
-  })
+  });
+
+  usersRef.on('value', function(snapshot){
+    $('#users').empty();
+
+    var users = snapshot.val();
+
+    var $lis = [];
+
+    for(var key in users){
+      var $li = $('<li>').text(users[key]);
+      $lis.push($li);
+    }
+    $('#users').append($lis);
+  });
+
+}
+
+function logout(){
+  usersRef.once('value', function(snapshot){
+    var users = snapshot.val();
+    for(var key in users){
+      if(users[key] === name){
+        usersRef.child(key).remove();
+      }
+    }
+    $("#output").hide();
+    $('.newMessage').hide();
+    $('.chooseName').show();
+
+  });
 }
 
 function start(){
   name = $('#name').val();
   $('#name').val('');
   if(name.length > 0 && name.length < 15){
+    $("#output").show();
     $('.newMessage').show();
     $('.chooseName').hide();
+    usersRef.push(name);
   } else {
     alert('Invalid name.');
   }
@@ -31,7 +69,7 @@ function start(){
 function addMessage(){
   var message = $("#message").val();
   $("#message").val('');
-  ref.push({
+  chatRef.push({
     name: name,
     time: Date.now(),
     message: message
